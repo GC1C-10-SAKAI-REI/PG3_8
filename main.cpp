792777e4a9f3ea58bdc2ea2a4c7b1c74dc610959
GC1C_10_サカイレイ <k019g1570@g.neec.ac.jp>
2023-06-09 13:01:47 +0900

    変数と関数の宣言(関数はプロ宣言のみ)

--- main.cpp
+++ main.cpp
@@ -9,8 +9,22 @@ typedef struct cell
 	struct cell* next;	//次のセル
 }CELL;
 
+//値を追加する関数
+void Create();
+//セルの一覧表示
+void Index();
+//任意の位置のアドレスまで辿る関数
+CELL *GetInsertListAddress();
+
 int main()
 {
+	//挿入する箇所
+	int iterator;
+	//挿入する値
+	int inputVal;
+	//
+	CELL *insertCell;
+
 	//先頭に内容が空のセルを宣言(先頭であることを明示する為)
 	CELL head;
 	head.prev = nullptr;
