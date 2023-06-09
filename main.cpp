3d826b0e0264aaa195f5cd44e2ff155c4f3a2b56
GC1C_10_サカイレイ <k019g1570@g.neec.ac.jp>
2023-06-09 12:52:01 +0900

    初期化

--- main.cpp
+++ main.cpp
@@ -1,26 +1,7 @@
 #include<stdio.h>
-#include<stdlib.h>
-
-//自己参照構造体
-typedef struct cell
-{
-	int val;			//
-	struct cell *prev;	//前のセル
-	struct cell *next;	//次のセル
-}CELL;
-
-//
-void Create();
-//
-void Index();
-//
-CELL *GetInsertListAddress();
 
 int main()
 {
-	int iterator;
-	int inputVal;
-	CELL *insertCell;
 
 	return 0;
 }
